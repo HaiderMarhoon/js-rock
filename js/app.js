664585f62dda3c5d2aff66d015f1b3f4be96544b
HaiderMarhoon <haidooer2003@gmail.com>
2025-06-29 11:09:08 +0300

    create the computer chioce fun

--- js/app.js
+++ js/app.js
@@ -18,8 +18,32 @@ const scissorsBntElement = document.querySelector("#scissors")
 
 
 /*-------------------------------- Functions --------------------------------*/
+play();
+
+function getComputerChoice(){
+    // generate a random choice 0 to 2
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    // select the item from the array
+    return choices[randomIndex];
+}
 
 
+
+// initialize game state 
+function play(){
+    computerChoice= getComputerChoice()
+    console.log(computerChoice)
+
+
+
+    // after update state , render to html
+    render()
+}
+
+// updated our UI/html directly
+function render (){
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
