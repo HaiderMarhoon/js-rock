90ab45a1dcfd67c9be2293b4d7b7901b461a06eb
HaiderMarhoon <haidooer2003@gmail.com>
2025-06-29 10:27:43 +0300

    Finish css

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const choices = ['rock', 'paper', 'scissors'];
+
+
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice;
+let computerChoice; 
+let msg;
+
+
+/*------------------------ Cached Element References ------------------------*/
+const resultDisplayEl = document.querySelector('#result-display');
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+console.log("is work")
